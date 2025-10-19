1c7ecf6d94af2f879d69cb73b353efd9f90b46b3
ramp-member <v279136@gmail.com>
2025-10-20 02:04:57 +0300

    Add files via upload

---
+++ 8.0.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+    void simvol() {
+        int count;
+        char symbol1, symbol2;
+
+        printf("Введите количество символов: ");
+        scanf("%d", &count);
+        printf("Введите символ для первой половины: ");
+        scanf(" %c", &symbol1);
+        printf("Введите символ для второй половины: ");
+        scanf(" %c", &symbol2);
+
+        printf("+");
+
+        for (int i = 1; i <= count; i++) {
+            if (i <= count / 2)
+                putchar(symbol1);
+            else
+                putchar(symbol2);
+
+            putchar('!');
+        }
+
+        printf("+\n");
+    }
+
+    int main() {
+        setlocale(LC_CTYPE, "RUS");
+        simvol();
+        return 0;
+    }
