6b47c2885524da245737b70bbcfaa0ea11acad8f
ramp-member <v279136@gmail.com>
2025-10-20 02:07:52 +0300

    Add files via upload

---
+++ 8.1.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+    void sum() {
+        int m, n, s = 0, k = 0;
+
+        printf("Введите m и n: ");
+        scanf("%d %d", &m, &n);
+
+        if (m < n) {
+            for (int i = m; i <= n; i++) {
+                s += i;
+                printf("Выполнено %d раз\n", ++k);
+            }
+        }
+        else {
+            for (int i = m; i >= n; i--) {
+                s += i;
+                printf("Выполнено %d раз\n", ++k);
+            }
+        }
+
+        printf("Результат: %d\n", s);
+    }
+
+    int main() {
+        setlocale(LC_CTYPE, "RUS");
+        sum();
+        return 0;
+    }
